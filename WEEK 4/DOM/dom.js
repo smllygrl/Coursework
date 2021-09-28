b958bc90393818dd12c930392affb38463374c23
smllygrl <hodgsonatalie@gmail.com>
2021-09-28 13:10:19 +1000

    Java Objects

---
+++ WEEK 4/DOM/dom.js
@@ -0,0 +1,12 @@
+// How to select HTML elements through the DOM
+
+// Syntax
+// const input = docuemnt.getElementByID("input");
+
+// AddEvent Listeners to Elements using DOM
+
+// const button = document.getElementById("button");
+
+// button.addEventListener("typeOfEvent", () =>  {
+// what you want to happen when the event happens
+// } )
