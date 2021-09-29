a7fc02c8298d7f86571d71ee3bcc5207b6ed2325
smllygrl <hodgsonatalie@gmail.com>
2021-09-29 13:38:15 +1000

    java objects and object iterators

---
+++ WEEK 4/OBJECT-ITERATION/objectiteration.js
@@ -0,0 +1,14 @@
+cosnt car = {
+    brand: "Honda",
+    model: "Civic",
+    release: 1999,
+    isManual: true,
+    colour: "silver",
+    wheels: 4,
+};
+
+let carKeys = Object.keys(cars);
+console.log("Keys in the car object", carKeys);
+console.log(carKeys.length);
+
+carKeys.push("petrolType";)
---
+++ WEEK 4/Objects/objects.js
@@ -0,0 +1,12 @@
+// What is an object?
+// How is it related to an array?
+
+// Arrays are good for storing data of the same type, like names
+// students = ["Blake", "Natalie", "Yujin"]
+
+// Key: value
+// BOTH = property
+
+// const coach = { name: "Aidan", age: "27", height: "185cm"}
+// console.log(coach.name); - returns Aidan
+// console.log(coach["name"]); - returns Aidan
